8e5df247bc8179db7e528607a979249fdb80a218
Alexander Schenk <106143485+AlexanderKlausSchenk@users.noreply.github.com>
2022-07-01 18:33:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//Navbar
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+
+//Navbarscroll
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
